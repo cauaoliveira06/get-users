bff64b6948f00b251dcbbfcc0fd632d1941ad407
Caua Oliveira <eucaua.oliveira06@gmail.com>
2026-07-23 21:32:22 -0300

    refactor : mover dados simulados para diretorio dedicado

---
+++ server/src/mocks/user.mock.js
@@ -0,0 +1,14 @@
+ const users = [{
+    name: 'Caua Oliveira',
+    age: '20',
+    status: true 
+}, {
+    name: 'Luana',
+    age: '21',
+    status: true
+}, {
+    name:'Rosilene',
+    age: '41',
+    status: false
+}];
+export default users;
