84012aef7d5d67f69fd2af27e8d779c186550373
Caua Oliveira <eucaua.oliveira06@gmail.com>
2026-08-19 21:08:14 -0300

    feat: atualizado scripts de comportamento de login

--- client/src/scripts/login.js
+++ client/src/scripts/login.js
@@ -1,6 +1,8 @@
 const email = document.querySelector('input[type="email"]');
 const password = document.querySelector('input[type="password"]');
 const button = document.querySelector('button');
+const checkbox = document.querySelector('input[type="checkbox"]');
+const labelPassword = document.querySelector('#placa');
 
 // dado mocado ( ficticio para verificação )
 /* const user = {
@@ -12,22 +14,35 @@ const button = document.querySelector('button');
 button.addEventListener('click', (event) => {
     event.preventDefault();//faz com que o botão não envie os dados e nem atualiza a tela
 
-    // caoptura o valor digitado no input
+    // captura o valor digitado no input
     const emailValue = email.value;
     const passwordValue = password.value;
 
     // validar se NÃO contem dados no input
-    if (!emailValue || !passwordValue){
-        alert ('Preencha todos os campos.');
+    if (!emailValue || !passwordValue) {
+        alert('Preencha todos os campos.');
         return;
     }
     // validar se email ou senha foi digitado incorreto comparando com o capturado na sessão temporaria do navegador usando 
     // sessionStorage
-    if (emailValue !== sessionStorage.getItem ('email') || passwordValue !== sessionStorage.getItem('password')){
+    if (emailValue !== sessionStorage.getItem('email') || passwordValue !== sessionStorage.getItem('password')) {
         alert('E-mail e/ou senha incorreto.');
         return;
     }
 
     alert('Acesso permitido');
-
 });
+checkbox.addEventListener('change', () => {
+    const AttributeValue = password.getAttribute('type')
+
+    if (AttributeValue === 'password') {
+        password.setAttribute('type', 'text');
+        labelPassword.textContent = 'Ocultar senha'
+        return
+    }
+
+
+    password.setAttribute('type', 'password');
+    labelPassword.textContent = 'Mostrar senha'
+
+})
